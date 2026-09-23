0808ea18c17861347c96352f14a8f7a02770594c
lqkshy <lqkshxy@gmail.com>
2026-09-23 23:21:08 +0530

    Fix some bugs

--- Nova2D/src/Game.cpp
+++ Nova2D/src/Game.cpp
@@ -3,77 +3,74 @@
 #include <SDL3/SDL.h>
 
 Game::Game() {
-	isRunning = false;
-	std::cout << "Game constructor called!" << std::endl;
+    isRunning = false;
+    std::cout << "Game constructor called!" << std::endl;
 }
 
 Game::~Game() {
-	std::cout << "Game destructor called!" << std::endl;
+    std::cout << "Game destructor called!" << std::endl;
 }
 
 void Game::Initialize() {
-	if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
-		std::cerr << "Error Initializing SDL." << std::endl;
-		return;
-	}
-	window = SDL_CreateWindow(
-		NULL,
-		SDL_WINDOWPOS_CENTERED,
-		SDL_WINDOWPOS_CENTERED,
-		800,
-		600,
-		SDL_WINDOW_BORDERLESS
-	);
-	if (!window) {
-		std::cerr << "Error creating SDL window." << std::endl;
-		return;
-	}
-	renderer = SDL_CreateRenderer(window, -1, 0);
-	if (!renderer) {
-		std::cerr << "Error creating SDL renderer." << std::endl;
-		return;
-	}
-	
-	isRunning = true;
+    if (!SDL_Init(SDL_INIT_VIDEO | SDL_INIT_EVENTS)) {
+        std::cerr << "Error Initializing SDL: " << SDL_GetError() << std::endl;
+        return;
+    }
+
+    window = SDL_CreateWindow("Nova2D", 800, 600, SDL_WINDOW_BORDERLESS);
+    if (!window) {
+        std::cerr << "Error creating SDL window: " << SDL_GetError() << std::endl;
+        return;
+    }
+
+    renderer = SDL_CreateRenderer(window, NULL);
+    if (!renderer) {
+        std::cerr << "Error creating SDL renderer: " << SDL_GetError() << std::endl;
+        return;
+    }
+
+    isRunning = true;
 }
 
 void Game::Run() {
-	while (isRunning) {
-		ProcessInput();
-		Update();
-		Render();
-	}
-
+    while (isRunning) {
+        ProcessInput();
+        Update();
+        Render();
+    }
 }
 
 void Game::ProcessInput() {
-	SDL_Event sdlEvent;
-	while (SDl_PollEvent(&sdlEvent)) {
-		switch (sdlEvent.type) {
-		case SDl_QUIT:
-			isRunning = false;
-			break;
-		case SDL_KEYDOWN:
-			if (sdlEvent.key.keysym.sym == SDLK_ESCAPE) {
-				isRunning = false;
-			}
-			break;
-		}
-	}
-
+    SDL_Event sdlEvent;
+    while (SDL_PollEvent(&sdlEvent)) {       
+        switch (sdlEvent.type) {
+        case SDL_EVENT_QUIT:                
+            isRunning = false;
+            break;
+        case SDL_EVENT_KEY_DOWN:             
+            if (sdlEvent.key.key == SDLK_ESCAPE) {  
+                isRunning = false;
+            }
+            break;
+        }
+    }
 }
 
 void Game::Update() {
-
+    // TODO: Update game objects...
 }
 
 void Game::Render() {
+    SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+    SDL_RenderClear(renderer);
 
+    // TODO: Render game objects...
+
+    SDL_RenderPresent(renderer);
 }
 
 void Game::Destroy() {
-	SDL_DestroyRenderer(renderer);
-	SDL_DestroyWindow(window);
-	SDL_Quit();
-
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
 }
--- Nova2D/src/Game.h
+++ Nova2D/src/Game.h
@@ -4,20 +4,20 @@
 #include <SDL3/SDL.h>
 
 class Game {
-	private
-		bool isRunning;
-		SDL_Window* window;
-		SDL_Renderer* renderer;
+private:                    
+    bool isRunning;
+    SDL_Window* window;
+    SDL_Renderer* renderer;
 
 public:
-	Game();
-	~Game();
-	void Initialize();
-	void Run();
-	void ProcessInput();
-	void Update();
-	void Render();
-	void Destroy();
+    Game();
+    ~Game();
+    void Initialize();
+    void Run();
+    void ProcessInput();
+    void Update();
+    void Render();
+    void Destroy();
 };
 
-#endif 
+#endif
